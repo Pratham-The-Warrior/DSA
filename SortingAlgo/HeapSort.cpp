8603235b616c07d7f494169d38b40d38ac1c8f98
Prathmesh Sarda <diamond123erock@gmail.com>
2025-09-11 22:59:34 +0530

    heapsort code

---
+++ SortingAlgo/HeapSort.cpp
@@ -0,0 +1,72 @@
+/* ************************************************
+ *                                                *
+ *       P     R     A     T     H     A     M    *
+ *                                                *
+ ************************************************ */
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to heapify a subtree rooted at index i
+void heapify(vector<int> &arr, int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+// Heap sort function
+void heapSort(vector<int> &arr)
+{
+    int n = arr.size();
+
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // Extract elements one by one
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        heapSort(arr);
+
+        for (int x : arr)
+            cout << x << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
